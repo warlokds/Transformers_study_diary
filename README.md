a43342ec8d7866b606c427470cccd4c2be5b135d
warlokds <2278044375@qq.com>
2026-01-13 00:25:05 +0800

    代码更新

--- README.md
+++ README.md
@@ -11,6 +11,18 @@ To support symlinks on Windows, you either need to activate Developer Mode or to
   warnings.warn(message)
 Device set to use cpu
 label:POSITIVE ,with score:0.9996
+###第一段（已注释部分1）
+
+### 第二段结果
+{'input_ids': [101, 151, 11157, 13195, 10158, 117, 10502, 151, 11157, 51875, 10772, 102], 'token_type_ids': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'attention_mask': [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]}
+input_ids:[[101, 151, 11157, 13195, 10158, 102, 0], [101, 10502, 151, 11157, 51875, 10772, 102]]
+token_type_ids:[[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
+attention_mask:[[1, 1, 1, 1, 1, 1, 0], [1, 1, 1, 1, 1, 1, 1]]
+SequenceClassifierOutput(loss=None, logits=tensor([[-1.8475, -1.8961, -0.4648,  1.0456,  2.5861],
+        [-1.7631, -1.0044,  0.4824,  0.9380,  1.0541]],
+       grad_fn=<AddmmBackward0>), hidden_states=None, attentions=None)
 
 ```
+
+#### tips:（1）logits是什么？
 
--- pipeline练习.py
+++ pipeline练习.py
@@ -1,4 +1,5 @@
 '''
+##1
 from transformers import pipeline##使用预训练最简单的方法是pipeline。transformers提供了现成的接口
 classifier= pipeline("sentiment-analysis")##example1:情感分析
 
@@ -6,3 +7,34 @@ results =classifier("I love yoy,but I love myself more")
 for result in results:
     print(f"label:{result['label']} ,with score:{round(result['score'],4)}")
 '''
+##2
+
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
+from transformers.modeling_outputs import SequenceClassifierOutput
+from torch import nn
+### 初始化，加载模型和分词器
+model_name="nlptown/bert-base-multilingual-uncased-sentiment"
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+classifier= pipeline('sentiment-analysis',model=model,tokenizer=tokenizer)
+input_text= tokenizer("I love yoy,but I love myself more")
+print(input_text)
+
+
+pt_batch =tokenizer(["I love yoy","but I love myself more"],
+                    padding=True,
+                    truncation=True,
+                    max_length=512,
+                    return_tensors="pt")
+for key, value in pt_batch.items():
+    print(f"{key}:{value.numpy().tolist()}")
+pt_outputs =model(**pt_batch)
+print(pt_outputs)
+
+
+pt_predictions=nn.Sequential(**pt_batch,labels=torch.tensor([1,0]) )
+print(pt_predictions)
+
+
